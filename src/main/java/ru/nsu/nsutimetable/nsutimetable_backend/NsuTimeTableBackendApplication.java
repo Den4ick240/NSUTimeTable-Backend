b0b9c5bf2a96296251d920399a4e5fea95efd5ed
Den4ick240 <zhigalovden@mail.ru>
2021-11-30 18:00:43 +0700

    changed cors configuration method

--- src/main/java/ru/nsu/nsutimetable/nsutimetable_backend/NsuTimeTableBackendApplication.java
+++ src/main/java/ru/nsu/nsutimetable/nsutimetable_backend/NsuTimeTableBackendApplication.java
@@ -26,7 +26,7 @@ public class NsuTimeTableBackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
             }
         };
     }
